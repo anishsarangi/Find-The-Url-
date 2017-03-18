f5797973b2f99ebc888367b9e875556f621bafb9
Anish Kumar Sarangi <anish.kumar.sarangi@gmail.com>
2017-03-19 01:00:02 +0530

    Update background.js

--- background.js
+++ background.js
@@ -3,8 +3,7 @@ chrome.runtime.onMessage.addListener(
     var url1;
     chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
     url1 = tabs[0].url;
-    console.log(url1);
-        if(request.greeting=="hello")
+     if(request.greeting=="hello")
     	sendResponse({farewell:url1});
 });
 return true;
